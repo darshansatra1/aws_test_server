ac90c9e96d7e7e71afec31c3130e91b9cf19cb8b
darshansatra1 <darshansatra1@gmail.com>
2021-07-11 18:11:10 +0530

    change port to 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var http = require("http");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 var server = http.createServer(app);
 var io = require("socket.io")(server);
