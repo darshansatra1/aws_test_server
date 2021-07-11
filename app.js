5a872ee9a0ef0d57490cb307c8d67a5a35b3fbce
darshansatra1 <darshansatra1@gmail.com>
2021-07-11 17:59:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+var http = require("http");
+const port = process.env.PORT || 5000;
+
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+app.use(express.json());
+
+io.on("Connection", (socket) => {
+    console.log("Connected");
+});
+
+app.get("/", (req, res) => {
+    console.log("CONNECTED");
+    res.send("");
+});
+
+server.listen(port, () => {
+    console.log(`Server started on Port ${port}`);
+});
