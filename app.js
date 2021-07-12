997ac4cd9dc4fa019856a40a8a689fad88dbc28b
darshansatra1 <darshansatra1@gmail.com>
2021-07-12 15:00:20 +0530

    change port to 3020

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 // var http = require("http");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 
 // var server = http.createServer(app);
 // var io = require("socket.io")(server, { cors: { origin: "*" } });
