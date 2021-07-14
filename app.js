51fde89a038cd76469a549a8f8e04f0b862e7ba3
darshansatra1 <darshansatra1@gmail.com>
2021-07-14 14:32:49 +0530

    debug

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ io.on('connection', function (client) {
 
 var server_port = process.env.PORT || 3030;
 server.listen(server_port, function (err) {
-    if (err) throw err
+    if (err) throw err;
     console.log('Listening on port %d', server_port);
 });
