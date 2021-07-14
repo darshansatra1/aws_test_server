0664a1f8ab059874b13bb9f61df52fae34dca106
darshansatra1 <darshansatra1@gmail.com>
2021-07-14 08:40:45 +0530

    port 3020

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ io.on('connection', function (client) {
     })
 })
 
-var server_port = process.env.PORT || 3000;
+var server_port = process.env.PORT || 3020;
 server.listen(server_port, function (err) {
     if (err) throw err
     console.log('Listening on port %d', server_port);
